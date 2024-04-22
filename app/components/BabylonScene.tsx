a90b4504a2e4bc4276ddc9b2e63ee9303db13522
nash1111 <nash1111@users.noreply.github.com>
2024-04-22 10:18:05 +0900

    chore: add mdx settings

--- app/components/BabylonScene.tsx
+++ app/components/BabylonScene.tsx
@@ -16,12 +16,7 @@ const BabylonScene = () => {
 
             const light = new HemisphericLight("light", new Vector3(1, 1, 0), scene);
 
-            SceneLoader.ImportMesh("", "/", "Neutral_M.obj", scene, function (meshes) {
-                scene.createDefaultCameraOrLight(true, true, true);
-                scene.createDefaultEnvironment();
-            });
-
-            SceneLoader.ImportMesh("", "/", "Neutral_M.obj", scene, function (meshes) {
+            SceneLoader.ImportMesh("", "", "Stanford_Bunny.stl", scene, function (meshes) {
                 scene.createDefaultCameraOrLight(true, true, true);
                 scene.createDefaultEnvironment();
             });
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,7 @@
 import { Button } from "./ui/button";
 import { Link } from "@remix-run/react";
+import React from "react";
+import { cn } from "~/lib/utils";
 import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger, navigationMenuTriggerStyle } from "~/components/ui/navigation-menu"
 
 
@@ -12,39 +14,51 @@ export const Header = () => {
                 </Link>
             </div>
             <div className="flex gap-4">
+                <Link to="https://nash1111-old-blog.pages.dev/">
+                    <Button variant="default">old blog</Button>
+                </Link>
                 <Link to="/blog">
                     <Button variant="default">Blog</Button>
                 </Link>
+
                 <NavigationMenu>
                     <NavigationMenuList>
                         <NavigationMenuItem>
-                            <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+                            <NavigationMenuTrigger>Components</NavigationMenuTrigger>
                             <NavigationMenuContent>
-                                <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                                    <li className="row-span-3">
-                                        <NavigationMenuLink asChild>
-                                            <a
-                                                className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                                                href="/"
-                                            >
-                                                <div className="mb-2 mt-4 text-lg font-medium">
-                                                    shadcn/ui
-                                                </div>
-                                                <p className="text-sm leading-tight text-muted-foreground">
-                                                    Beautifully designed components that you can copy and
-                                                    paste into your apps. Accessible. Customizable. Open
-                                                    Source.
-                                                </p>
-                                            </a>
-                                        </NavigationMenuLink>
-                                    </li>
+                                <ul className="grid gap-3 p-4 grid-cols-1">
+                                    {components.map((component) => (
+                                        <div
+                                            key={component.id}
+                                        >
+                                            {component.title}
+                                        </div>
+                                    ))}
                                 </ul>
                             </NavigationMenuContent>
                         </NavigationMenuItem>
 
                     </NavigationMenuList>
                 </NavigationMenu>
+
             </div>
         </header>
     );
 };
+
+const components: { id: number, title: string; href: string; description: string }[] = [
+    {
+        id: 1,
+        title: "Alert",
+        href: "/docs/primitives/alert-dialog",
+        description:
+            "A modal dialog that interrupts the user with important content and expects a response.",
+    },
+    {
+        id: 2,
+        title: "Hover",
+        href: "/docs/primitives/hover-card",
+        description:
+            "For sighted users to preview content available behind a link.",
+    },
+]
