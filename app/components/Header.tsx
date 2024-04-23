304fb22e8456c69b0619f88590c6111567c7053c
nash1111 <nash1111@users.noreply.github.com>
2024-04-23 19:36:13 +0900

    feat: add view transition

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,7 +9,7 @@ export const Header = () => {
     return (
         <header className="flex justify-between items-center p-4 bg-gradient-to-r from-cyan-500 to-blue-700">
             <div className="flex gap-4">
-                <Link to="/">
+                <Link prefetch="intent" to="/" unstable_viewTransition>
                     <Button variant="default">nash1111</Button>
                 </Link>
             </div>
@@ -17,18 +17,25 @@ export const Header = () => {
                 <Link to="https://nash1111-old-blog.pages.dev/">
                     <Button variant="default">old blog</Button>
                 </Link>
-                <Link to="/blog">
+                <Link to="/blog" prefetch="intent" unstable_viewTransition>
                     <Button variant="default">Blog</Button>
+                </Link>
+                <Link to="/about" prefetch="intent" unstable_viewTransition>
+                    <Button variant="default">about</Button>
                 </Link>
 
                 <NavigationMenu>
                     <NavigationMenuList>
                         <NavigationMenuItem>
                             <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-                            <NavigationMenuContent>
-                                <ul className="grid gap-3 p-4 grid-cols-1">
+                            <NavigationMenuContent
+                                className={cn(
+                                    "min-w-[16rem] left-0 top-0 w-full data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 md:absolute md:w-auto",
+                                )}
+                            >
+                                <ul className="grid gap-3 p-4 grid-cols-1 overflow-auto">
                                     {components.map((component) => (
-                                        <div
+                                        <div className="break-words"
                                             key={component.id}
                                         >
                                             {component.title}
