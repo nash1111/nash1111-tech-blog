a70a10bb49e8a7111a016c7f810feed7029d2792
nash1111 <nash1111@users.noreply.github.com>
2024-04-29 14:54:55 +0900

    feat: add public

--- app/root.tsx
+++ app/root.tsx
@@ -38,7 +38,7 @@ export default function App() {
     <div className="flex flex-col min-h-screen">
       <Header />
       <div className="flex-grow">
-        <div>てすと</div>
+        <Outlet />
       </div>
       <Footer />
     </div>);
