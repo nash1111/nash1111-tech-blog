f13530ee3e3e8e2c38f2f869726c769020240243
nash1111 <nash1111@users.noreply.github.com>
2024-05-02 21:50:35 +0900

    fix: ui

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -32,14 +32,10 @@ export const Header = () => {
                     <NavigationMenuList>
                         <NavigationMenuItem>
                             <NavigationMenuTrigger>Others</NavigationMenuTrigger>
-                            <NavigationMenuContent
-
-                            >
-                                <ul className="grid gap-3 p-4 grid-cols-1 overflow-auto">
+                            <NavigationMenuContent>
+                                <ul className="flex flex-col gap-3 p-4 overflow-x-auto">
                                     {components.map((component) => (
-                                        <div className="break-words"
-                                            key={component.id}
-                                        >
+                                        <div className="break-words" key={component.id}>
                                             {component.title}
                                         </div>
                                     ))}
@@ -59,17 +55,11 @@ const components: { id: number, title: string; href: string; description: string
     {
         id: 1,
         title: "Tasks",
-        href: "/docs/primitives/alert-dialog",
-        description:
-            "A modal dialog that interrupts the user with important content and expects a response.",
-        closed: true
+        href: "/task",
     },
     {
         id: 2,
         title: "Snippets",
         href: "/docs/primitives/hover-card",
-        description:
-            "For sighted users to preview content available behind a link.",
-        closed: true
     },
 ]
