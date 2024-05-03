92e11d1e9b9b99018c57818129606dc6def0e8b8
nash1111 <nash1111@users.noreply.github.com>
2024-05-03 14:47:18 +0900

    wip: fixing mdx frontmatter

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -1,9 +1,15 @@
 import { Outlet } from "@remix-run/react";
 
+import * as postFirst from "./blog.first.mdx";
+
 export default function Component() {
-  return (
-    <div className="p-10 prose">
-      <Outlet />
-    </div>
-  );
+    console.log(postFirst);
+    return (
+        <div className="p-10 prose">
+            <Outlet />
+            {JSON.stringify(postFirst)}
+            {postFirst.frontmatter}
+            <postFirst.default />
+        </div>
+    );
 }
