b0087e41c761996707de4b1be7b309a81e1c23da
nash1111 <nash1111@users.noreply.github.com>
2024-06-15 16:13:46 +0900

    wip

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -1,14 +1,22 @@
-import { Link, Outlet } from "@remix-run/react";
+import { Link, Outlet, useLocation } from "@remix-run/react";
 import { Button } from "~/components/ui/button";
-import { ChevronLeft } from "lucide-react";
+import { ChevronLeft, Twitter } from "lucide-react";
 
 export default function Component() {
+    const location = useLocation();
+    const tweetUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(`https://yourdomain.com${location.pathname}`)}`;
+
     return (
         <div className="p-10 prose md:container mx-auto">
             <Outlet />
-            <Link to="/blog" prefetch="intent" unstable_viewTransition>
-                <Button variant="link"><ChevronLeft className="w-4 h-4 mr-2" />Back to BLOG</Button>
-            </Link>
+            <div className="flex justify-between items-center">
+                <Link to="/blog" prefetch="intent" unstable_viewTransition>
+                    <Button variant="link"><ChevronLeft className="w-4 h-4 mr-2" />Back to BLOG</Button>
+                </Link>
+                <a href={tweetUrl} target="_blank" rel="noopener noreferrer">
+                    <Button variant="primary"><Twitter className="w-4 h-4 mr-2" />Tweet</Button>
+                </a>
+            </div>
         </div>
     );
 }
