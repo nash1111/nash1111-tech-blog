cda10fca4eb0f954814ba66900310501846d95c1
nash1111 <nash1111@users.noreply.github.com>
2024-06-23 20:00:55 +0900

    feat: add BlogImage components

---
+++ app/components/BlogImage.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface BlogImageProps {
+    imagePath: string;
+    altText?: string;
+    width?: string;
+    height?: string;
+}
+
+const BlogImage: React.FC<BlogImageProps> = ({ imagePath, altText = 'Blog Image', width, height }) => {
+    return (
+        <div className="blog-image-container" style={{ width, height }}>
+            <img src={imagePath} alt={altText} className="blog-image" style={{ width: '100%', height: '100%' }} />
+        </div>
+    );
+};
+
+export default BlogImage;
