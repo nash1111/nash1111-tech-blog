562cadfb0a1a97eb562b019b6c3b00d56ccc8c5b
nash1111 <nash1111@users.noreply.github.com>
2024-06-24 07:19:12 +0900

    refactor: split blogposts

---
+++ app/lib/posts.ts
@@ -0,0 +1,10 @@
+import * as postFirst from "~/routes/blog.nexttoremix.mdx";
+import * as postHowTaskPageCreated from "~/routes/blog.howtaskpagecreated.mdx";
+import * as postWhatICareAbout from "~/routes/blog.whaticareabout.mdx";
+import * as postPrAgent from "~/routes/blog.pragent.mdx";
+export const posts = [
+  { path: "/blog/nexttoremix", data: postFirst },
+  { path: "/blog/howtaskpagecreated", data: postHowTaskPageCreated },
+  { path: "/blog/whaticareabout", data: postWhatICareAbout },
+  { path: "/blog/pragent", data: postPrAgent },
+];
--- app/routes/blog_._index.tsx
+++ app/routes/blog_._index.tsx
@@ -1,16 +1,8 @@
-import * as postFirst from "./blog.nexttoremix.mdx";
-import * as postHowTaskPageCreated from "./blog.howtaskpagecreated.mdx";
-import * as postWhatICareAbout from "./blog.whaticareabout.mdx";
-import * as postPrAgent from "./blog.pragent.mdx";
+
 import { BlogCard } from "~/components/BlogCard";
+import { posts } from "~/lib/posts";
 
 export default function BlogListComponent() {
-    const posts = [
-        { path: "/blog/nexttoremix", data: postFirst },
-        { path: "/blog/howtaskpagecreated", data: postHowTaskPageCreated },
-        { path: "/blog/whaticareabout", data: postWhatICareAbout },
-        { path: "/blog/pragent", data: postPrAgent }
-    ];
 
     return (
         <div className="p-10 prose">
