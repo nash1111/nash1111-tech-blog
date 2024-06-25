d7f9fdaf1f20fa37cb577b3788accd698e4adef3
nash1111 <nash1111@users.noreply.github.com>
2024-06-25 21:51:49 +0900

    feat: add smartphone screenshot

--- tests/task.spec.tsx
+++ tests/task.spec.tsx
@@ -1,8 +1,17 @@
 import { test } from '@playwright/test';
 
 test.describe('/task page', () => {
-    test('should render without crashing', async ({ page }) => {
+    test('take screen shot of task page', async ({ page }) => {
         await page.goto('http://localhost:5173/task');
-        await page.screenshot({ path: `test-results/screenshots/screenshot-${Date.now()}.png` });
+        await page.screenshot({ path: `test-results/screenshots/task-page.png` });
+    });
+});
+
+test.describe('/blog page', () => {
+    test('take screen shot of blog page', async ({ page }) => {
+        await page.goto('http://localhost:5173/blog');
+        await page.screenshot({ path: `test-results/screenshots/blog-page.png` });
+        await page.setViewportSize({ width: 375, height: 667 }); // iPhone 6/7/8 dimensions
+        await page.screenshot({ path: `test-results/screenshots/blog-page-iphone.png` });
     });
 });
