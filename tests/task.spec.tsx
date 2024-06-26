bc2fbcd06dca8ff37cd6457bb595501a376398ad
nash1111 <nash1111@users.noreply.github.com>
2024-06-26 16:41:33 +0900

    feat: add post

--- tests/task.spec.tsx
+++ tests/task.spec.tsx
@@ -1,17 +1,15 @@
 import { test } from '@playwright/test';
 
 test.describe('/task page', () => {
-    test('take screen shot of task page', async ({ page }) => {
+    test('take screen shot of task page', async ({ page }, testInfo) => {
         await page.goto('http://localhost:5173/task');
-        await page.screenshot({ path: `test-results/screenshots/task-page.png` });
+        await page.screenshot({ path: `test-results/screenshots/task-page-${testInfo.project.name}.png` });
     });
 });
 
 test.describe('/blog page', () => {
-    test('take screen shot of blog page', async ({ page }) => {
+    test('take screen shot of blog page', async ({ page }, testInfo) => {
         await page.goto('http://localhost:5173/blog');
-        await page.screenshot({ path: `test-results/screenshots/blog-page.png` });
-        await page.setViewportSize({ width: 375, height: 667 }); // iPhone 6/7/8 dimensions
-        await page.screenshot({ path: `test-results/screenshots/blog-page-iphone.png` });
+        await page.screenshot({ path: `test-results/screenshots/blog-page-${testInfo.project.name}.png` });
     });
 });
