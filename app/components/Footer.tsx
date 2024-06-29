41eaccd39608fb7a02ac83d7caf86adbaa29369a
nash1111 <nash1111@users.noreply.github.com>
2024-06-29 14:23:26 +0900

    feat: add rss page

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,9 @@
+import { Rss } from "lucide-react";
+
 export const Footer = () => {
     return (
         <footer className="flex justify-center items-center p-4 bg-gradient-to-r from-cyan-500 to-blue-700">
-            <p className="text-white">© nash1111</p>
+            <p className="text-white flex items-center">© nash1111 <a href="/rss" className="ml-2"><Rss /></a></p>
         </footer>
     );
 };
---
+++ app/routes/rss.tsx
@@ -0,0 +1,34 @@
+import { LoaderFunction } from "@remix-run/cloudflare";
+import { useLoaderData, useNavigate } from "@remix-run/react";
+import fs from "fs";
+import { ChevronLeft } from "lucide-react";
+import path from "path";
+import { xml2js } from "xml-js";
+import { Button } from "~/components/ui/button";
+
+export const loader: LoaderFunction = () => {
+    const rssPath = path.join(process.cwd(), "public", "rss.xml");
+    const rssContent = fs.readFileSync(rssPath, "utf-8");
+    const rssJson = xml2js(rssContent, { compact: true });
+    return rssJson;
+}
+
+export default function Rss() {
+    const rssData = useLoaderData();
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <Button variant="link" onClick={() => navigate(-1)}>
+                <ChevronLeft className="w-4 h-4 mr-2" />
+                Back
+            </Button>
+            <pre>{JSON.stringify(rssData, null, 2)}</pre>
+            <Button variant="link" onClick={() => navigate(-1)}>
+                <ChevronLeft className="w-4 h-4 mr-2" />
+                Back
+            </Button>
+
+        </>
+    );
+}
