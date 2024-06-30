dd2ffc4c3a28ca1d02e9eceb82feb84c1158c023
nash1111 <nash1111@users.noreply.github.com>
2024-06-30 16:02:38 +0900

    wip: duplicated mp4

--- tests/recordBlogViewTransition.spec.tsx
+++ tests/recordBlogViewTransition.spec.tsx
@@ -3,18 +3,20 @@ import { test } from '@playwright/test';
 test.use({
     video: {
         mode: 'on',
-        saveOnFailure: false // デフォルトのビデオ保存を無効にする
     }
 });
 test.describe('/task page', () => {
-    test('should render without crashing', async ({ page }) => {
+    test('should render without crashing', async ({ page }, testInfo) => {
         await page.goto('http://localhost:5173/blog');
+        await page.waitForSelector('text=個人ブログをNextからRemixに移行しました');
         await page.getByText('個人ブログをNextからRemixに移行しました').click();
-        // TODO: avoid waitForTimeout
-        await page.waitForTimeout(1000);
+        await page.waitForLoadState('networkidle');
+        await page.keyboard.press('ArrowDown');
+        await page.waitForSelector('text=Back to BLOG');
+        await page.getByText('Back to BLOG').click();
+        await page.waitForLoadState('networkidle');
         await page.close();
-        await page.video()?.saveAs(`test-results/videos/blogPage.webm`);
+        await page.video()?.saveAs(`test-results/videos/${testInfo.project.name}/blogPage.webm`);
         // webm->gif
     });
 });
-
