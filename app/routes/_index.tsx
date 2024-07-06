5c0d95a596230316ac9d02abfbcd3034460603eb
nash1111 <nash1111@users.noreply.github.com>
2024-07-06 18:17:50 +0900

    chore: add padding

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -27,7 +27,7 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8", padding: "24px" }}>
       <Link to="/blog" prefetch="intent" unstable_viewTransition>
         <Button variant="default">Blog</Button>
       </Link>
