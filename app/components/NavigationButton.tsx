ae22aa05bf7c74f315adc720d0f1d370d8e51eb7
nash1111 <nash1111@users.noreply.github.com>
2025-04-27 20:22:38 +0900

    chore: unstable->stable

--- app/components/NavigationButton.tsx
+++ app/components/NavigationButton.tsx
@@ -10,7 +10,7 @@ interface NavigationButtonProps {
 //    const to = `/images/${idx}`;
 //    const vt = unstable_useViewTransitionState(href);
 //    return (
-//      <Link to={to} unstable_viewTransition>
+//      <Link to={to} viewTransition>
 //        <img
 //          src={src}
 //          alt={alt}
@@ -25,7 +25,7 @@ interface NavigationButtonProps {
 export const NavigationButton = ({ path, label }: NavigationButtonProps) => {
     return (
         <div className="flex gap-4">
-            <Link prefetch="intent" to={path} unstable_viewTransition>
+            <Link prefetch="intent" to={path} viewTransition>
                 <Button variant="default">{label}</Button>
             </Link>
         </div>
